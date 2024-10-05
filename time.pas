160cbe13373913604648d439da3acaccaa781f27
Никита Горбатов <nikita058526@gmail.com>
2024-10-05 09:03:39 +0300

    Задание 2

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿##
+var n := ReadInteger('Введите год:');
+if (n mod 4 = 0) then
+begin 
+  if (n mod 100 = 0) and (n mod 400 <> 0) then
+    print($'{n} - не високосный год')
+  else print($'{n} -  високосный год')
+end;
+if (n mod 4 <> 0) then print($'{n} - не високосный год');
