861a7139e79f8516a2896029df1d6c719595cfde
Никита Горбатов <nikita058526@gmail.com>
2024-10-05 09:07:33 +0300

    Задание 2

--- time.pas
+++ time.pas
@@ -3,7 +3,6 @@ var n := ReadInteger('Введите год:');
 if (n mod 4 = 0) then
 begin 
   if (n mod 100 = 0) and (n mod 400 <> 0) then
-    print($'{n} - не високосный год')
-  else print($'{n} -  високосный год')
-end;
-if (n mod 4 <> 0) then print($'{n} - не високосный год');
+    print($'{n} - не високосный год, дней в году {n*365}')
+  else print($'{n} -  високосный год, дней в году {n*366}')
+end else print($'{n} - не високосный год, дней в году {n*365}');
